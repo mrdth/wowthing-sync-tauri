c22f77d21f4f3d857b550709f670975599b9a51b
mrdth <mrdth@zeventien.org>
2024-12-22 20:22:10 +0000

    Update to Tauri v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+import { defineConfig } from 'vite';
+
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
